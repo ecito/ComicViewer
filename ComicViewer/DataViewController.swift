2933b6b8eaefd78a84ea29d831ed632530718fbb
Andre Navarro <andre.navarro@mlb.com>
2019-08-19 18:18:43 +0200

    Initial Commit

---
+++ ComicViewer/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  ComicViewer
+//
+//  Created by Andre Navarro on 8/19/19.
+//  Copyright © 2019 DML. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+  @IBOutlet weak var dataLabel: UILabel!
+  var dataObject: String = ""
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.dataLabel!.text = dataObject
+  }
+
+
+}
+
